0d8e45e5a466725e002345cd9b079cadc6376b7d
ryicoh <ryicoh@gmail.com>
2020-03-26 16:39:46 +0900

    ３回試行するようにした

--- go/start.sh
+++ go/start.sh
@@ -2,14 +2,22 @@
 
 /entrypoint.sh mysqld &
 
-for i in `seq 1 20`
-do
-  mysqladmin ping && \
-    exec "$@" && \
-    exit 0
+num_attempt=30
+num_connected=0
+success_threshold=3
+
+for i in `seq 1 $num_attempt`; do
+  if mysqladmin ping -h 127.0.0.1 &> /dev/null; then
+    num_connected=$(($num_connected+1))
+
+    if [[ $num_connected == $success_threshold ]]; then
+        exec "$@"
+        exit 0
+    fi
+  fi
 
   sleep 1
 done
 
-echo "mysql did not start within 20 seconds"
+echo "mysql did not start within $num_attempt seconds"
 exit 1
